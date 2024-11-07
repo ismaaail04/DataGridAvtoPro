71e6e1ee7c3b35d42bd8fcc7e78fb5337680b9f7
I_ism <i_ismov_04@mail.rru>
2024-11-07 22:05:14 +0300

    Добавьте файлы проекта.

---
+++ DataGridAvto.Contracts/Models/ICarStats.cs
@@ -0,0 +1,17 @@
+﻿namespace DataGridAvto.Contracts.Models
+{
+    /// <summary>
+    /// Интерфейс для вычисляемых данных о списке <see cref="Avto"/>
+    /// </summary>
+    public interface ICarStats
+    {
+        /// <summary>
+        /// Общее количество авто
+        /// </summary>
+        int Count { get; }
+        /// <summary>
+        /// Количество авто с критически низким уровенем запаса хода
+        /// </summary>
+        int LowCount { get; }
+    }
+}
---
+++ DataGridAvto/Program.cs
@@ -0,0 +1,29 @@
+﻿using DataGridAvto.AvtoManager;
+using DataGridAvto.StorageMemory;
+using System;
+using System.Windows.Forms;
+using Microsoft.Extensions.Logging;
+
+namespace DataGridAvto
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var factory = LoggerFactory.Create(builder => builder.AddDebug());
+            var logger = factory.CreateLogger(nameof(DataGrid));
+
+            var storage = new MemoryCarStorage();
+            var manager = new CarManager(storage, logger);
+
+            Application.Run(new Form1(manager));
+        }
+    }
+}
